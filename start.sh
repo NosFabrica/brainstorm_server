a471a4a2cbedbcc2aa76a120d420304214be3378
en <enes@nostrdev.com>
2026-04-10 19:36:00 +0200

    chore: drop direct python exec in start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 poetry run alembic upgrade head
-exec $(poetry env info --path)/bin/python -m uvicorn app.api:app --host 0.0.0.0 --port 8000
+poetry run uvicorn app.api:app --host 0.0.0.0 --port 8000
