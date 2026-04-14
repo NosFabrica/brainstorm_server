13d40b7de0f29a3ce8ca8dc1fd535d050811fa3d
kowalski <kowalski@ft.hn>
2026-04-14 15:40:56 +0100

    trigger specific graperank endpoint

--- app/routers/brainstorm_pubkey/router.py
+++ app/routers/brainstorm_pubkey/router.py
@@ -1,7 +1,10 @@
+from app.schemas.schemas import BrainstormRequestInstance
+from app.services.brainstorm_request_service import create_brainstorm_request
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession as AsyncDBSession
 
 from app.core.database import get_db
+from app.repos.brainstorm_nsec import select_brainstorm_nsec_by_pubkey_on_db
 from app.schemas.request_response_schemas import BrainstormPubkeyResponse
 from app.services.brainstorm_pubkey_service import get_or_create_brainstorm_pubkey
 
@@ -22,3 +25,25 @@ async def get_brainstorm_pubkey_endpoint(
 ) -> BrainstormPubkeyResponse:
     result = await get_or_create_brainstorm_pubkey(db, nostr_pubkey)
     return BrainstormPubkeyResponse(data=result)
+
+
+@router.post(
+    path="/{nostr_pubkey}/trigger_graperank",
+    tags=[],
+    dependencies=[],
+    summary="trigger graperank for a specific pubkey",
+)
+async def trigger_brainstorm_pubkey_graperank_endpoint(
+    nostr_pubkey: str,
+    db: AsyncDBSession = Depends(dependency=get_db),
+) -> BrainstormRequestInstance:
+    await select_brainstorm_nsec_by_pubkey_on_db(db, pubkey=nostr_pubkey)
+
+    triggered_graperank: BrainstormRequestInstance = await create_brainstorm_request(
+        db=db,
+        algorithm="graperank",
+        parameters=nostr_pubkey,
+        pubkey=nostr_pubkey,
+        nsec_exists=True,
+    )
+    return BrainstormPubkeyResponse(data=triggered_graperank)
